31d6a8281a2157f71220b2ee85f1601b4031bf96
masooma-raza <masoomaraza2002@gmail.com>
2024-10-12 15:53:05 +0500

    Practice

---
+++ foam/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace foam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = textBox1.Text;
+            string password = textBox2.Text;
+
+            string correctEmail = "laibakhan12@gmail.com";
+            string correctPassword = "12345";
+
+            if (email == correctEmail && password== correctPassword)
+            {
+                MessageBox.Show("Login Successfull", "success",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invaild emailor password. please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+    }
+}
